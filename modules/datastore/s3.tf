3a47a620e735e5976ed40887b1388f2bd6eb6e64
Edgard Zelaya <ezelaya@toogoodtogo.com>
2025-09-18 15:41:41 +0200

    PLA-9566 add-support-iam-authentication - Fixing deprecation

--- modules/datastore/s3.tf
+++ modules/datastore/s3.tf
@@ -1,14 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket        = local.s3_bucket_name
   force_destroy = var.force_destroy_s3_bucket
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.s3.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
 
   tags = merge(
     var.standard_tags,
@@ -21,6 +13,17 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
